affb8d6fe54c906b1b2532c2da43417b58c04595
Veryan Goodship <veryan.goodship@truba.news>
2020-02-27 21:13:32 -0500

    Enhanced Form Control

--- src/app/staff/staff-modal/staff-modal.component.ts
+++ src/app/staff/staff-modal/staff-modal.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AbstractControl, FormGroup, FormBuilder, Validators, ValidationErrors } from '@angular/forms';
-import { Staff } from '../../types/staff.type';
+import { Staff } from '../../shared/types/staff.type';
 
 
 @Component({
@@ -10,7 +10,6 @@ import { Staff } from '../../types/staff.type';
   styleUrls: ['./staff-modal.component.css']
 })
 export class StaffModalComponent implements OnInit {
-  @Input() name;
 
   groups: string[] = ['doctor', 'nurse', 'admin']
   staffForm: FormGroup;
@@ -27,24 +26,52 @@ export class StaffModalComponent implements OnInit {
     this.createForm();
   }
 
-  //Form builder will give you more validation control on the Input, less simple but better long term.
+  //Form builder will gives more validation control on the Input, less simple but safer long term.
   createForm() {
     this.staffForm = this.formBuilder.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       group: ['', [Validators.required]],
-  });
-  this.firstNameControl = this.staffForm.controls['firstName'];
-  this.lastNameControl = this.staffForm.controls['lastName'];
-  this.groupControl = this.staffForm.controls['group'];
+    },
+    {
+      validator: this.staffValidator('firstName', 'lastName','group')
+    });
+    this.firstNameControl = this.staffForm.controls['firstName'];
+    this.lastNameControl = this.staffForm.controls['lastName'];
+    this.groupControl = this.staffForm.controls['group'];
   }
 
-  getNewStaff(): Staff | void{
+  staffValidator(firstNameKey:string, lastNameKey:string, groupKey:string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let specialChar = /[ `!@#$%^&*()_+=\[\]{};:"\\|,.<>\/?~]/;
+      let hasNumber = /\d/
+      let firstName = group.controls[firstNameKey];
+      let lastName = group.controls[lastNameKey];
+      let staffGroup = group.controls[groupKey];
+      if (specialChar.test(firstName.value) || hasNumber.test(firstName.value)) {
+        return {
+          invalidFirstName: true
+        }
+      }
+      if (specialChar.test(lastName.value) || hasNumber.test(lastName.value)) {
+        return {
+          invalidLastName: true
+        };
+      }
+      if (!this.groups.includes(staffGroup.value)) {
+        return {
+          groupNotFound: true
+        };
+      }
+    }
+  }
+
+  getNewStaff(): Staff | void {
     if(!this.staffForm.valid) {
       this.getFormValidationErrors();
       return null;
     }
-    let staff:Staff = {
+    let staff: Staff = {
       id: 0,
       firstName: this.firstNameControl.value,
       lastName: this.lastNameControl.value,
@@ -56,13 +83,13 @@ export class StaffModalComponent implements OnInit {
   // Left here for debugging purposes, should be remove for production
   getFormValidationErrors() {
     Object.keys(this.staffForm.controls).forEach(key => {
-    const controlErrors: ValidationErrors = this.staffForm.get(key).errors;
-    if (controlErrors != null) {
-          Object.keys(controlErrors).forEach(keyError => {
-            console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
-          });
-        }
-      });
-    }
+      const controlErrors: ValidationErrors = this.staffForm.get(key).errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+        });
+      }
+    });
+  }
 
 }
